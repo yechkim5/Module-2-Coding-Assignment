5bda70a37e55d18da3a1f27409d747a6268e36e9
yechkim5 <yech.kim@mail.utoronto.ca>
2025-10-16 15:57:50 -0400

    Update Main.java
    Fixed error return value

--- src/main/java/dogapi/Main.java
+++ src/main/java/dogapi/Main.java
@@ -29,7 +29,7 @@ public class Main {
             return subBreeds.size();
         }
         catch (BreedFetcher.BreedNotFoundException e) {
-            return -1;
+            return 0;
         }
 
     }
